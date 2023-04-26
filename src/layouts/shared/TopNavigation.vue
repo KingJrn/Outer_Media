5119ee00fe7dc923b7d3373ad0bc20efecd2cf06
KingJrn <emmajnr503@protonmail.com>
2023-04-26 13:45:25 +0100

    added fonts, and completed topNavigation component

--- src/layouts/shared/TopNavigation.vue
+++ src/layouts/shared/TopNavigation.vue
@@ -1,14 +1,82 @@
 <template>
-    <header class="header">
-        kkkkkkkkkkk
-    </header>
+    <nav class="w-100">
+        <div class="page-title">Dashboard</div>
+        <div class="live-section">
+            <!-- live section -->
+            <div class="user">
+                <div class="user-name">Budda Live</div>
+                <div class="user-category">Admin</div>
+            </div>
+            <!-- logout -->
+            <div class="logout-container">
+                <div class="logout">Logout</div>
+                <div class="logout-icon">
+                    <img src="@/assets/images/vectors/logout.svg" alt="logout-image" />
+                </div>
+            </div>
+        </div>
+    </nav>
 </template>
 <script>
-    export default{
-        name: 'TopNavigation'
-    }
+export default {
+    name: 'TopNavigation'
+}
 </script>
 <style lang="scss" scoped>
-@import "@/assets/scss/main.scss";
+@import '@/assets/scss/main.scss';
 
+nav {
+    background: $color-brand-primary;
+    box-shadow: 0px 4px 15px $nav-box-shadow-color;
+    border: $default-border $nav-border-color;
+    border-radius: $default-border-radius;
+    @include flexed-Container(row, space-between);
+    height: 54px;
+    padding-inline: $standard-padding;
+}
+
+.page-title {
+    @include textStyles('Poppins', 600, 16px, 20px);
+    color: $nav-text-primary-bold;
+    letter-spacing: $default-letter-sapcing;
+
+}
+
+.live-section {
+    display: flex;
+    @include flexed-Container;
+    gap: $default-gap * 18px;
+
+}
+
+.user {
+    .user-name {
+        @include textStyles('Poppins', 500, 14px, 21px);
+        letter-spacing: $default-letter-sapcing;
+        color: $nav-text-primary-dark;
+    }
+
+    .user-category {
+        @include textStyles('Poppins', 400, 11px, 16px);
+        color: $nav-text-primary-success;
+    }
+}
+
+.logout-container {
+    background-color: $color-brand-sceondary-danger;
+    @include flexed-Container;
+    width: 71px;
+    height: 30px;
+    padding: $default-spacing (
+        $default-spacing * 2
+    );
+border-radius: $default-border-radius + 3px;
+gap: $default-gap * 3px;
+
+
+.logout {
+    @include textStyles('Poppins', 600, 10px, 20px);
+    color: $color-brand-primary;
+}
+}
 </style>
