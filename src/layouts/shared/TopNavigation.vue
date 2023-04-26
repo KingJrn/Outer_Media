d9ea66dd7792fb82978a5d4c6f5462d2066547a7
KingJrn <emmajnr503@protonmail.com>
2023-04-26 10:49:27 +0100

    added color palattes and topNavigation component

---
+++ src/layouts/shared/TopNavigation.vue
@@ -0,0 +1,14 @@
+<template>
+    <header class="header">
+        kkkkkkkkkkk
+    </header>
+</template>
+<script>
+    export default{
+        name: 'TopNavigation'
+    }
+</script>
+<style lang="scss" scoped>
+@import "@/assets/scss/main.scss";
+
+</style>
---
+++ src/pages/Customer/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <TopNavigation>
+
+  </TopNavigation>
+</template>
+
+<script>
+import TopNavigation from '@/layouts/shared/TopNavigation.vue';
+export default {
+  name: 'Home',
+  components: {
+    TopNavigation
+  }
+}
+</script>
