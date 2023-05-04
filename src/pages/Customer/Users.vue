85ba8aea1e78d5479378d7f2ad2f55df21f8c48e
KingJrn <emmajnr503@protonmail.com>
2023-05-04 17:24:57 +0100

    added user

---
+++ src/pages/Customer/Users.vue
@@ -0,0 +1,133 @@
+<template>
+    <mainView>
+        <div class="main-container">
+            <!-- select user components  -->
+            <selectUsers />
+
+            <!-- table -->
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Username</th>
+                        <th scope="col">Added</th>
+                        <th scope="col">Last Login</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td scope="row">16</td>
+                        <td>Matt Ross</td>
+                        <td>Buddahlive</td>
+                        <td>Jul, 31, 2023</td>
+                        <td>Never</td>
+                        <td colspan="2" class="actions">
+                            <span class="edit">Edit</span>
+                            <img class="table-icon" src="@/assets/images/vectors/trash.svg" alt="trash" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td scope="row">16</td>
+                        <td>Matt Ross</td>
+                        <td>Buddahlive</td>
+                        <td>Jul, 31, 2023</td>
+                        <td>Never</td>
+                        <td colspan="2" class="actions">
+                            <span class="edit">Edit</span>
+                            <img class="table-icon" src="@/assets/images/vectors/trash.svg" alt="trash" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td scope="row">16</td>
+                        <td>Matt Ross</td>
+                        <td>Buddahlive</td>
+                        <td>Jul, 31, 2023</td>
+                        <td>Never</td>
+                        <td colspan="2" class="actions">
+                            <span class="edit">Edit</span>
+                            <img class="table-icon" src="@/assets/images/vectors/trash.svg" alt="trash" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td scope="row">16</td>
+                        <td>Matt Ross</td>
+                        <td>Buddahlive</td>
+                        <td>Jul, 31, 2023</td>
+                        <td>Never</td>
+                        <td colspan="2" class="actions">
+                            <span class="edit">Edit</span>
+                            <img class="table-icon" src="@/assets/images/vectors/trash.svg" alt="trash" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td scope="row">16</td>
+                        <td>Matt Ross</td>
+                        <td>Buddahlive</td>
+                        <td>Jul, 31, 2023</td>
+                        <td>Never</td>
+                        <td colspan="2" class="actions">
+                            <span class="edit">Edit</span>
+                            <img class="table-icon" src="@/assets/images/vectors/trash.svg" alt="trash" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td scope="row">16</td>
+                        <td>Matt Ross</td>
+                        <td>Buddahlive</td>
+                        <td>Jul, 31, 2023</td>
+                        <td>Never</td>
+                        <td colspan="2" class="actions">
+                            <span class="edit">Edit</span>
+                            <img class="table-icon" src="@/assets/images/vectors/trash.svg" alt="trash" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td scope="row">16</td>
+                        <td>Matt Ross</td>
+                        <td>Buddahlive</td>
+                        <td>Jul, 31, 2023</td>
+                        <td>Never</td>
+                        <td colspan="2" class="actions">
+                            <span class="edit">Edit</span>
+                            <img class="table-icon" src="@/assets/images/vectors/trash.svg" alt="trash" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td scope="row">16</td>
+                        <td>Matt Ross</td>
+                        <td>Buddahlive</td>
+                        <td>Jul, 31, 2023</td>
+                        <td>Never</td>
+                        <td colspan="2" class="actions">
+                            <span class="edit">Edit</span>
+                            <img class="table-icon" src="@/assets/images/vectors/trash.svg" alt="trash" />
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <!-- pagination -->
+            <pagination />
+        </div>
+    </mainView>
+</template>
+
+<script>
+import mainView from '@/layouts/partials/mainView.vue'
+import selectUsers from '@/layouts/partials/selectUsers.vue'
+import pagination from '@/layouts/partials/pagination.vue'
+export default {
+    name: 'Users',
+    components: {
+        mainView,
+        selectUsers,
+        pagination
+    }
+}
+</script>
+<style lang="scss" scoped>
+@import '@/assets/scss/main.scss';
+@import '@/assets/scss/shared/table.scss';
+</style>
