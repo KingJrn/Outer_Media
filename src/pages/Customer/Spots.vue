543ca6157fe6dbf28ddf8858c94fd7d6433e340f
KingJrn <emmajnr503@protonmail.com>
2023-05-05 01:11:47 +0100

    added spot components

---
+++ src/pages/Customer/Spots.vue
@@ -0,0 +1,228 @@
+<template>
+    <mainView>
+        <div class="main-container">
+            <!-- select user components  -->
+            <selectUsers />
+
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Last Modified</th>
+                        <th scope="col">Background Image</th>
+                        <th scope="col" class="action-header">Action </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td scope="row">233</td>
+                        <td>Wednesday Karaoke</td>
+                        <td>Dec 22, 2021 04:12 pm EST</td>
+                        <td>
+                            <img class="table-icon" src="@/assets/images/vectors/flowers.svg" alt="flowers" />
+
+                        </td>
+                        <td colspan="5" class="actions">
+                            <span class="vertical-line"></span>
+                            <img class="table-icon" src="@/assets/images/vectors/trash.svg" alt="trash" />
+
+                            <span class="edit">Edit</span>
+                            <span class="packages">Assign</span>
+                            <span class="play-view">
+                                <span class="view">View</span>
+                                <span>
+                                    <img class="table-icon play img-fluid " src="@/assets/images/vectors/play.svg"
+                                        alt="play-button" />
+                                </span>
+                            </span>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td scope="row">233</td>
+                        <td>Wednesday Karaoke</td>
+                        <td>Dec 22, 2021 04:12 pm EST</td>
+                        <td>
+                            <img class="table-icon" src="@/assets/images/vectors/flowers.svg" alt="flowers" />
+
+                        </td>
+                        <td colspan="5" class="actions">
+                            <span class="vertical-line"></span>
+                            <img class="table-icon" src="@/assets/images/vectors/trash.svg" alt="trash" />
+
+                            <span class="edit">Edit</span>
+                            <span class="packages">Assign</span>
+                            <span class="play-view">
+                                <span class="view">View</span>
+                                <span>
+                                    <img class="table-icon play img-fluid " src="@/assets/images/vectors/play.svg"
+                                        alt="play-button" />
+                                </span>
+                            </span>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td scope="row">233</td>
+                        <td>Wednesday Karaoke</td>
+                        <td>Dec 22, 2021 04:12 pm EST</td>
+                        <td>
+                            <img class="table-icon" src="@/assets/images/vectors/flowers.svg" alt="flowers" />
+
+                        </td>
+                        <td colspan="5" class="actions">
+                            <span class="vertical-line"></span>
+                            <img class="table-icon" src="@/assets/images/vectors/trash.svg" alt="trash" />
+
+                            <span class="edit">Edit</span>
+                            <span class="packages">Assign</span>
+                            <span class="play-view">
+                                <span class="view">View</span>
+                                <span>
+                                    <img class="table-icon play img-fluid " src="@/assets/images/vectors/play.svg"
+                                        alt="play-button" />
+                                </span>
+                            </span>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td scope="row">233</td>
+                        <td>Wednesday Karaoke</td>
+                        <td>Dec 22, 2021 04:12 pm EST</td>
+                        <td>
+                            <img class="table-icon" src="@/assets/images/vectors/flowers.svg" alt="flowers" />
+
+                        </td>
+                        <td colspan="5" class="actions">
+                            <span class="vertical-line"></span>
+                            <img class="table-icon" src="@/assets/images/vectors/trash.svg" alt="trash" />
+
+                            <span class="edit">Edit</span>
+                            <span class="packages">Assign</span>
+                            <span class="play-view">
+                                <span class="view">View</span>
+                                <span>
+                                    <img class="table-icon play img-fluid " src="@/assets/images/vectors/play.svg"
+                                        alt="play-button" />
+                                </span>
+                            </span>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td scope="row">233</td>
+                        <td>Wednesday Karaoke</td>
+                        <td>Dec 22, 2021 04:12 pm EST</td>
+                        <td>
+                            <img class="table-icon" src="@/assets/images/vectors/flowers.svg" alt="flowers" />
+
+                        </td>
+                        <td colspan="5" class="actions">
+                            <span class="vertical-line"></span>
+                            <img class="table-icon" src="@/assets/images/vectors/trash.svg" alt="trash" />
+
+                            <span class="edit">Edit</span>
+                            <span class="packages">Assign</span>
+                            <span class="play-view">
+                                <span class="view">View</span>
+                                <span>
+                                    <img class="table-icon play img-fluid " src="@/assets/images/vectors/play.svg"
+                                        alt="play-button" />
+                                </span>
+                            </span>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td scope="row">233</td>
+                        <td>Wednesday Karaoke</td>
+                        <td>Dec 22, 2021 04:12 pm EST</td>
+                        <td>
+                            <img class="table-icon" src="@/assets/images/vectors/flowers.svg" alt="flowers" />
+
+                        </td>
+                        <td colspan="5" class="actions">
+                            <span class="vertical-line"></span>
+                            <img class="table-icon" src="@/assets/images/vectors/trash.svg" alt="trash" />
+
+                            <span class="edit">Edit</span>
+                            <span class="packages">Assign</span>
+                            <span class="play-view">
+                                <span class="view">View</span>
+                                <span>
+                                    <img class="table-icon play img-fluid " src="@/assets/images/vectors/play.svg"
+                                        alt="play-button" />
+                                </span>
+                            </span>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td scope="row">233</td>
+                        <td>Wednesday Karaoke</td>
+                        <td>Dec 22, 2021 04:12 pm EST</td>
+                        <td>
+                            <img class="table-icon" src="@/assets/images/vectors/flowers.svg" alt="flowers" />
+
+                        </td>
+                        <td colspan="5" class="actions">
+                            <span class="vertical-line"></span>
+                            <img class="table-icon" src="@/assets/images/vectors/trash.svg" alt="trash" />
+
+                            <span class="edit">Edit</span>
+                            <span class="packages">Assign</span>
+                            <span class="play-view">
+                                <span class="view">View</span>
+                                <span>
+                                    <img class="table-icon play img-fluid " src="@/assets/images/vectors/play.svg"
+                                        alt="play-button" />
+                                </span>
+                            </span>
+                        </td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </div>
+    </mainView>
+</template>
+
+<script>
+import mainView from '@/layouts/partials/mainView.vue'
+import selectUsers from '@/layouts/partials/selectUsers.vue'
+export default {
+    name: 'Spots',
+    components: {
+        mainView,
+        selectUsers,
+    },
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/main.scss';
+@import '@/assets/scss/shared/table.scss';
+
+.vertical-line {
+
+    width: 28px;
+    height: 0px;
+    border: $default-border-2 $table-border-color;
+    transform: rotate(90deg);
+}
+
+.action-header {
+    position: relative;
+    left: 6.5rem;
+}
+</style>
