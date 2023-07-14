39c835f5c30933542556cc901393bb0175a89cc8
KingJrn <emmajnr503@protonmail.com>
2023-07-14 17:01:00 +0100

    plugging in (add new user)

--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -1,43 +1,61 @@
 import axios from 'axios'
+import { error } from 'jquery'
 
 // Base url
-const BASE_URL = 'https://api.outer.media/api';
+const BASE_URL = 'https://api.outer.media/api'
 
-const LOGIN_PATH = () => "/login";
-const ADD_ORGANIZATION_PATH = () => "/organisations";
-const GET_ORGANIZATION_PATH = () => "/organisations";
+const LOGIN_PATH = () => '/login'
+const ADD_ORGANIZATION_PATH = () => '/organisations'
+const GET_ORGANIZATION_PATH = () => '/organisations'
 
+// Organizations  dashboard
+const ADD_NEW_USER_PATH = (id) => `/${id}/users`
 
-export default{
-   loginUser: function (loginData, successCallback, errorCallback) {
-      axios
-        .post(BASE_URL + LOGIN_PATH(), loginData)
-        .then((response) => {
-          successCallback(response.data);
-        })
-        .catch((response) => {
-          errorCallback(response);
-        });
-    },
-    addOrganization: function (data, callback) {
-      axios
-        .post(BASE_URL + ADD_ORGANIZATION_PATH(), data)
-        .then((response) => {
-          callback(response.data);
-        })
-        .catch((error) => {
-          callback(error.data);
-        });
-    },
-    getOrganizations: function (callback) {
-      axios
-        .get(BASE_URL + GET_ORGANIZATION_PATH())
-        .then((response) => {
-          callback(response.data);
-          console.log(response.data)
-        })
-        .catch((error) => {
-          callback(error.data);
-        });
-    },
+export default {
+  loginUser: function (loginData, successCallback, errorCallback) {
+    axios
+      .post(BASE_URL + LOGIN_PATH(), loginData)
+      .then((response) => {
+        successCallback(response.data)
+      })
+      .catch((response) => {
+        errorCallback(response)
+      })
+  },
+  addOrganization: function (data, callback) {
+    axios
+      .post(BASE_URL + ADD_ORGANIZATION_PATH(), data)
+      .then((response) => {
+        callback(response.data)
+      })
+      .catch((error) => {
+        callback(error.data)
+      })
+  },
+  getOrganizations: function (callback) {
+    axios
+      .get(BASE_URL + GET_ORGANIZATION_PATH())
+      .then((response) => {
+        callback(response.data)
+        // console.log(response.data)
+      })
+      .catch((error) => {
+        callback(error.data)
+      })
+  },
+
+  // ! This is for ORGANIZATION DASHBOARD
+
+  addNewUser: function(data) {
+    const username = userData.allData.username;
+    axios
+      .post(BASE_URL + ADD_NEW_USER_PATH(`${username}`), data)
+      .then((response) => {
+        callback(response.data)
+      })
+      .catch((error) => {
+        callback(error.data)
+      })
+  }
+  
 }
--- src/views/AddNewUser.vue
+++ src/views/AddNewUser.vue
@@ -1,53 +1,96 @@
 <template>
-    <mainView>
-        <div class="conatiner">
-            <div class="header-sect">
-                <h2>Add New User</h2>
-                <p>Please enter User details below to create account</p>
+  <mainView>
+    <div class="conatiner">
+      <div class="header-sect">
+        <h2>Add New User</h2>
+        <p>Please enter User details below to create account</p>
+      </div>
+      <div class="form-data">
+        <form action="" @submit.prevent="">
+          <div class="d-flex edit-group w-100">
+            <div class="mb-3 col">
+              <label for="exampleInputEmail1" class="form-label">Users Real Name</label>
+              <input
+                type="text"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                required
+                placeholder="Placeholder"
+                v-bind="name"
+              />
             </div>
-            <div class="form-data">
-                <form action="" @submit.prevent="">
-                    <div class="d-flex edit-group w-100">
-                        <div class="mb-3 col">
-                            <label for="exampleInputEmail1" class="form-label">Users Real Name</label>
-                            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required placeholder="Placeholder">
-                        </div>
-                        <div class="mb-3 col">
-                            <label for="exampleInputEmail1" class="form-label">UserName</label>
-                            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required placeholder="Placeholder">
-                        </div>
-
-                    </div>
-                    <div class="d-flex edit-group w-100">
-                        <div class="mb-3 col">
-                            <label for="exampleInputEmail1" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                        </div>
-                        <div class="mb-3 col">
-                            <label for="exampleInputEmail1" class="form-label">Confirm Password</label>
-                            <input type="password" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            
-                        </div>
-
-                    </div>
-                    
-                    <button class="add" type="button">Create Account</button>
-                </form>
+            <div class="mb-3 col">
+              <label for="exampleInputEmail1" class="form-label">UserName</label>
+              <input
+                type="text"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                required
+                placeholder="Placeholder"
+                v-bind="username"
+              />
             </div>
-        </div>
-    </mainView>
+          </div>
+          <div class="d-flex edit-group w-100">
+            <div class="mb-3 col">
+              <label for="exampleInputEmail1" class="form-label">Password</label>
+              <input
+                type="password"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                v-bind="password"
+              />
+            </div>
+            <div class="mb-3 col">
+              <label for="exampleInputEmail1" class="form-label">Confirm Password</label>
+              <input
+                type="password"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                v-bind="confirm_password"
+              />
+            </div>
+          </div>
+
+          <button class="add" type="button" @click="createAccount()">Create Account</button>
+        </form>
+      </div>
+    </div>
+  </mainView>
 </template>
 
 <script>
 import mainView from '@/layouts/partials/mainView.vue'
-
+import apiServices from '@/services/apiServices.js'
 export default {
-    name: 'AddNewUser',
-    components: {
-        mainView
-    },
-
-
+  name: 'AddNewUser',
+  components: {
+    mainView
+  },
+  data() {
+    return {
+      requestData: {
+        name: '',
+        username: '',
+        password: '',
+        confirm_password: ''
+      }
+    }
+  },
+  mounted() {
+    console.log(this.userData)
+  },
+  methods: {
+    createAccount() {
+      apiServices.addNewUser(this.requestData, (response) => {
+        console.log(response.data)
+      })
+    }
+  }
 }
 </script>
 
